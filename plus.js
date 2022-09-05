28ce723e5810a6ac2248e673837f867438ec28c7
whrodhkd123 <102003109+whrodhkd123@users.noreply.github.com>
2022-09-05 11:34:10 +0900

    Add files via upload
    9월5일3교시

---
+++ plus.js
@@ -0,0 +1,11 @@
+function plus(num1,num2){
+    let result;                     /*let 저장소 이름을 가르키는 키워드*/
+result=num1+num2;
+return result
+}
+function mai(num1,num2){
+    let result;                     
+result=num1-num2;
+return result
+}
+/*js를 이용해서 파일 불러와서 넣을 수 있음*/ 
